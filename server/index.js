ce0d93bcb1e9d632d2b1439b0fb17ab6962ef128
osama <osamaalsrraj3@gmail.com>
2025-07-30 04:07:19 +0300

    update server package.json

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173" ,"https://upsales.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
