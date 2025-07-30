8e0df7a34e67710497721e7a1537acff2b1ce804
osama <osamaalsrraj3@gmail.com>
2025-07-30 04:09:14 +0300

    set the deployed server api

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:4000/api",
+    baseURL: "https://upsales-api.vercel.app/api",
     headers: {
         "Content-Type": "application/json",
     },
